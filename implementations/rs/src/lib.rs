d6a85784cb7830449ec64ff59c3cf1cb02c5387c
Cesar <cesarbrazon10@gmail.com>
2023-06-13 23:20:27 +0200

    chore(rs): fix sign typed data

--- implementations/rs/src/lib.rs
+++ implementations/rs/src/lib.rs
@@ -107,7 +107,8 @@ impl Module for EthereumWalletPlugin {
                 let signer = connection.get_signer().unwrap();
                 let typed_data: TypedData = from_value(parameters[1].clone()).unwrap();
                 let hash = Runtime::block_on(&runtime, signer.sign_typed_data(&typed_data));
-                Ok(hash.unwrap().to_string())
+                let hash = hash.unwrap().to_string();
+                to_string(&hash).map_err(PluginError::JSONError)
             }
             "eth_sendTransaction" => {
                 let signer = connection.get_signer().unwrap();
